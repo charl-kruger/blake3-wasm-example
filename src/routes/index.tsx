dc26f3f8f857f4e3fe97818ec038236cd1926cb6
Charl Kruger <nit3watch@gmail.com>
2023-03-07 09:29:07 +0200

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,10 @@ import type { DocumentHead } from '@builder.io/qwik-city';
 
 import * as blake3 from 'blake3/browser-async';
 
-blake3.load().then(() => {
-  console.log(blake3.hash('hello world'));
-});
+// blake3.load().then(() => {
+//   console.log(blake3.hash('hello world'));
+// });
 
-blake3.load().then(() => {
-  console.log(blake3.hash('hello world'));
-});
 
 export default component$(() => {
   return (
@@ -17,6 +14,18 @@ export default component$(() => {
       <h1>
         Blake3-wasm <span class="lightning">⚡️</span>
       </h1>
+
+      <button onClick$={async () => {
+        // const test = await blake3.default('hello world')
+        // console.log(test)
+        const hash = await blake3.load()
+        const valueToHash = 'hello world';
+
+        // hash the value using blake3
+        const result = hash(valueToHash);
+        console.log(result)
+      }
+      }>do some blake3 stuff</button>
     </div>
   );
 });
